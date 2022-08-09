007c48223c38ce79b8a1a9ede8ebb57af2fd647a
jmyoung <schabdachj@chop.edu>
2022-08-09 09:42:22 -0400

    updated to generate PNGs from the preprocess but not FS'ed images

--- runPngGenerator.py
+++ runPngGenerator.py
@@ -21,7 +21,7 @@ def checkSliceHasTissue(brainSlice):
     totalPixels = len(brainSlice)*len(brainSlice[0])
     nonzeroPixels = np.count_nonzero(brainSlice)
     
-    if (nonzeroPixels >= 0.05*totalPixels):
+    if (nonzeroPixels >= 0.07*totalPixels):
         return True
     else:
         return False
@@ -86,18 +86,18 @@ def selectBrainSlices(fn):
     return [dim0Slices, dim1Slices, dim2Slices]
 
 
-def generatePngsSingleScan(FS_folder, subject, outputDir):
+def generatePngsSingleScan(mprFn, subject, outputDir):
     freeview_command = 'freeview -cmd {cmd} '
-    cmd_txt = """ -v {anatomy}:grayscale=10,100 -f {lh_wm}:color=blue:edgecolor=blue -f {rh_wm}:color=blue:edgecolor=blue -f {lh_pial}:color=red:edgecolor=red -f {rh_pial}:color=red:edgecolor=red
-     """  
+#    cmd_txt = """ -v {anatomy}:grayscale=10,100 -f {lh_wm}:color=blue:edgecolor=blue -f {rh_wm}:color=blue:edgecolor=blue -f {lh_pial}:color=red:edgecolor=red -f {rh_pial}:color=red:edgecolor=red """  
+    cmd_txt = """ -v {anatomy}:grayscale=10,500  """
 
     # To step through the sagittal slices this is added for every slice. 
     dim0_slice = ' -slice {xpos} 127 127 \n -ss {opfn} \n  '  
-    dim1_slice = ' -slice 127 127 {xpos} \n -ss {opfn} \n  '  
-    dim2_slice = ' -slice 127 {xpos} 127 \n -ss {opfn} \n  '  
+    dim1_slice = ' -slice 127 {xpos} 127 \n -ss {opfn} \n  '  
+    dim2_slice = ' -slice 127 127 {xpos} \n -ss {opfn} \n  '  
 
     # Set up variables 
-    mprFn = os.path.join(FS_folder, 'mri', 'norm.mgz')          # Want to use the images without the face
+#    mprFn = os.path.join(FS_folder, 'mri', 'norm.mgz')          # Want to use the images without the face
     target_directory = outputDir
     os.makedirs(target_directory, exist_ok=True)
     cmd_file_x = os.path.join(target_directory, 'cmd_x.txt')
@@ -107,10 +107,10 @@ def generatePngsSingleScan(FS_folder, subject, outputDir):
     # Start writing the command string
     sj_cmd = cmd_txt.format(
         anatomy=mprFn,
-        lh_wm=os.path.join(FS_folder, 'surf', 'lh.white'),
-        lh_pial=os.path.join(FS_folder, 'surf', 'lh.pial'),
-        rh_wm=os.path.join(FS_folder, 'surf', 'rh.white'),
-        rh_pial=os.path.join(FS_folder, 'surf', 'rh.pial'),
+#        lh_wm=os.path.join(FS_folder, 'surf', 'lh.white'),
+#        lh_pial=os.path.join(FS_folder, 'surf', 'lh.pial'),
+#        rh_wm=os.path.join(FS_folder, 'surf', 'rh.white'),
+#        rh_pial=os.path.join(FS_folder, 'surf', 'rh.pial'),
         subject=subject
     )
 
@@ -163,14 +163,21 @@ def generatePngsSingleScan(FS_folder, subject, outputDir):
 
 
 def main():
-    subjDir = "/Users/youngjm/Data/clip/images/derivatives/mpr_fs_reconall_6.0.0/"
-    fn = '/Users/youngjm/Data/clip/images/derivatives/mpr_fs_reconall_6.0.0_tables/mpr_fs_reconall_6.0.0_structural_stats.csv'
+#    subjDir = "/Users/youngjm/Data/clip/images/derivatives/mpr_fs_reconall_6.0.0/"
+#    fn = '/Users/youngjm/Data/clip/images/derivatives/mpr_fs_reconall_6.0.0_tables/mpr_fs_reconall_6.0.0_structural_stats.csv'
+    fsSubjDir = "/Volumes/youngjm_ext/Data/clip/derivatives/mpr_fs_preprocWashUACPCAlignment_defaced_6.0.0/"
+    ifsSubjDir = "/Volumes/youngjm_ext/Data/clip/derivatives/mpr_ifs_preprocWashUACPCAlignment_defaced_6.0.0/"
+    fn = '/Users/youngjm/Data/clip/fs6_stats/fs6_structural_stats.csv'
     outputDir = "/Users/youngjm/Data/clip/images/qc/mpr_fs_6.0.0/pngs"
 
     df = pd.read_csv(fn)
     
     # Get the ids of the scans we want graded
     scanIds = df['scan_id'].values
+
+#    scanIds = glob.glob(subjDir+"**/anat/**.nii.gz", recursive=True)
+    # Drop scan ids with contrast
+    scanIds = [i for i in scanIds if "-03_T1w" not in i and "-04_T1w" not in i and "-05_T1w" not in i and "-06_T1w" not in i]
     
     # For each scan
     for scanId in scanIds:
@@ -178,14 +185,24 @@ def main():
         # Identify the complete path to the scan
         subj = scanId.split("_")[0]
         ses = scanId.split("_")[1]
+        age = int(ses.split("age")[1])
+
+        if age > 1096:
+            fn = fsSubjDir
+        else:
+            fn = ifsSubjDir
+
+        fn += subj+"/"+ses+"/"+scanId+"/acpc_final.nii.gz"
+        print(fn)
 
         # if there are already pngs for the scan
         existingPngs = glob.glob(os.path.join(outputDir, subj+"_"+ses+"*.png"))
         if len(existingPngs) < 9:
             print("generating image slices for", subj, ses)
-            scanPath = subjDir+subj+"/"+ses+"/anat/"+scanId+"/"
-            generatePngsSingleScan(scanPath, subj+"_"+ses, outputDir)
+            generatePngsSingleScan(fn, subj+"_"+ses, outputDir)
+
 
 
 if __name__ == "__main__":
     main()
+    print("DONE")
