3d67e2302e8f250e58af60d82e45c07f3e13e789
jmyoung <schabdachj@chop.edu>
2022-08-11 13:50:51 -0400

    updated png generation

--- generatePngBatches.py
+++ generatePngBatches.py
@@ -1,15 +1,19 @@
 import os
+import shutil
 import random
 
 # Variables to change
-baseDir = "/home/jenna/CHOP/png_testing/"
-numScansPerBatch = 9
+baseDir = "/Users/youngjm/Data/clip/images/qc/mpr_fs_6.0.0/"
+# Assuming 5 seconds per png and 9 pngs per scan, ~1 minute per scan
+# Keeping the batch to ~30 minute period, want 30 scans per batch
+numPngsPerBatch = 9*30 
 
 # Get the paths for the images 
 imgs = sorted(os.listdir(baseDir+"pngs/"))
+imgs = [ i for i in imgs if "sub" in i ]
 
-# Get the subjects from the list of images
-subjects = list(set([i.split("_")[0] for i in imgs]))
+# Get the subjects with session ids from the list of images
+subjects = list(set([i.split("_dim")[0] for i in imgs]))
 random.shuffle(subjects)
 
 # Set up variables
@@ -22,9 +26,9 @@ subjIdx = 0
 # while there are subjects who have not been assigned to a batch
 while len(assignedSubjects) < len(subjects):
     # fill a batch with subject pngs
-    print(" batch start")
-    while len(batchFiles) < numScansPerBatch:
-        print(assignedSubjects)
+    print("batch start")
+    # While there are still spaces left in the batch and scans left to assign
+    while len(batchFiles) < numPngsPerBatch and subjIdx < len(subjects):
         if subjects[subjIdx] not in assignedSubjects:
             print(subjects[subjIdx])
             # Get the ids of the scans belonging to the subject
@@ -34,8 +38,7 @@ while len(assignedSubjects) < len(subjects):
             assert(len(subjImgs) == 9)
     
             # Add the images for that subject to the list of images for the batch
-            batchFiles.extend(subjImgs)   # move the files into the batch folder
-            print(batchFiles)
+            batchFiles.extend(subjImgs)   
             assignedSubjects.append(subjects[subjIdx])
 
             # Increment the subject idx
@@ -44,12 +47,15 @@ while len(assignedSubjects) < len(subjects):
     # When the batch is full
     # move the files from baseDir + png/ to baseDir + batc - how am I keeping track of batch ids? Maybe each batch gets 2 4 digit ids? batch_0000_0001 
     outDir = baseDir+"batch_0000_"+str(batchId).zfill(4)+"/"
-    # print stuff
     print(outDir)
-    print(batchFiles)
-    print(batchId)
     # If the outdir doesn't exist make it
-    # Copy or move....
+    if not os.path.exists(outDir):
+        os.mkdir(outDir)
+    # Copy files over to the batch directory
+    for fn in batchFiles:
+        print(fn)
+        newFn = outDir + fn.split("/")[-1]
+        shutil.copyfile(fn, newFn)
     # Reset variables for next batch
     batchFiles = []
     batchId += 1
