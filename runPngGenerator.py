fa3f650ad80ace79c8cd9359ee67ababf4d03c8f
Jenna Schabdach <jmschabdach@gmail.com>
2024-03-26 15:48:23 -0400

    skeleton code

--- runPngGenerator.py
+++ runPngGenerator.py
@@ -1,13 +1,15 @@
 import subprocess as sb
 import os
 import glob
-import nibabel
 import numpy as np
 import random
 import pandas as pd
 import sys
 import argparse
+import json
 import nibabel as nib
+from nilearn import plotting
+from nilearn.image import coord_transform
 
 def selectSliceIndices(brainMin, brainMax):
     randRange = int(round((brainMax - brainMin)*0.05))
@@ -30,6 +32,64 @@ def checkSliceHasTissue(brainSlice):
         return False
 
 
+def selectNewSliceCoordinates(fn):
+    # Load the masked brain image
+    nibImg = nibabel.load(fn)
+    img = nibImg.get_fdata()
+
+    # Goal: identify volume of brain that's non-zero
+    # Get the upper and lower bounds of brain in each dimension
+    dim0Lims = np.where(img.any(axis=(1, 2)))[0]
+    dim1Lims = np.where(img.any(axis=(0, 2)))[0]
+    dim2Lims = np.where(img.any(axis=(0, 1)))[0]
+
+    # Get the min and max of each dimension
+    dim0MinBrain = sorted(set(dim0Lims))[0]
+    dim0MaxBrain = sorted(set(dim0Lims))[-1]
+    
+    dim1MinBrain = sorted(set(dim1Lims))[0]
+    dim1MaxBrain = sorted(set(dim1Lims))[-1]
+    
+    dim2MinBrain = sorted(set(dim2Lims))[0]
+    dim2MaxBrain = sorted(set(dim2Lims))[-1]
+
+    # Dimension 0
+    dim0Slices = selectSliceIndices(dim0MinBrain, dim0MaxBrain)
+    k = 1
+    while not all([checkSliceHasTissue(img[i, :, :]) for i in dim0Slices]):
+        print("Regenerating dim0 slices")
+        if k % 5 == 0:
+            dim0MinBrain += 1
+            dim0MaxBrain -= 1
+        k += 1
+        dim0Slices = selectSliceIndices(dim0MinBrain, dim0MaxBrain)
+    
+    # Dimension 1
+    dim1Slices = selectSliceIndices(dim1MinBrain, dim1MaxBrain)
+    k = 1
+    while not all([checkSliceHasTissue(img[:, i, :]) for i in dim1Slices]):
+        print("Regenerating dim1 slices")
+        if k % 5 == 0:
+            dim1MinBrain += 1
+            dim1MaxBrain -= 1
+        k += 1
+        dim1Slices = selectSliceIndices(dim1MinBrain, dim1MaxBrain)
+    
+    # Dimension 2
+    dim2Slices = selectSliceIndices(dim2MinBrain, dim2MaxBrain)
+    k = 1
+    while not all([checkSliceHasTissue(img[:, :, i]) for i in dim2Slices]):
+        print("Regenerating dim2 slices")
+        if k % 5 == 0:
+            dim2MinBrain += 1
+            dim2MaxBrain -= 1
+        k += 1
+        dim2Slices = selectSliceIndices(dim2MinBrain, dim2MaxBrain)
+    
+    return [dim0Slices, dim1Slices, dim2Slices], nibImg.affine
+
+
+
 def selectBrainSlices(fn):
     # Load the masked brain image
     nibImg = nibabel.load(fn)
@@ -46,11 +106,6 @@ def selectBrainSlices(fn):
     dim0Lims = np.where(img.any(axis=(1, 2)))[0]
     dim1Lims = np.where(img.any(axis=(0, 2)))[0]
     dim2Lims = np.where(img.any(axis=(0, 1)))[0]
-    # LIES - This order of coordinates works with PSR+ orientations  
-#    dim1Lims = np.where(img.any(axis=(1, 2)))[0]
-#    dim2Lims = np.where(img.any(axis=(0, 2)))[0]
-#    dim0Lims = np.where(img.any(axis=(0, 1)))[0]
-
 
     # Get the min and max of each dimension
     dim0MinBrain = sorted(set(dim0Lims))[0]
@@ -102,22 +157,54 @@ def selectBrainSlices(fn):
     
     return [dim0Slices, dim1Slices, dim2Slices], [dim0MidBrain, dim1MidBrain, dim2MidBrain]
 
+def generatePngsSingleScanNibabel(mprFn, subject, outputDir):
+    brainSlices, affine = selectBrainSlices(mprFn)
 
-def generatePngsSingleScan(mprFn, subject, outputDir):
+    # Dim0 in pixel space corresponds to coronal view (dim1) in MNI space
+    for dim0Slice in brainSlices[0]:
+        newSlice = coord_transform(dim0Slice, 0, 0, affine)
+        newFn = subject+"_coronal_slice"+str(int(newSlice[1])).zfill(3), output_file=newFn+".png"
+        plotting.plot_anat(nibImg, display_mode="y", cut_coords=[tmp[1]], draw_cross = False, output_file=newFn)
+ 
+
+    for dim1Slice in brainSlices[1]:
+        pass
+
+    for dim2Slice in brainSlices[2]:
+        pass
+   
+    print("PNGs generated for", subject)
+
+
+
+def generatePngsSingleScanFreeView(mprFn, subject, outputDir):
     freeview_command = 'freeview -cmd {cmd} '
     cmd_txt = """ -v {anatomy}:grayscale=10,500  """
- 
-#    # This order of coordinates works with LAS+ orientations
-#    dim0_slice = ' -slice {xpos} {ymid} {zmid} \n -ss {opfn} \n  '  
-#    dim1_slice = ' -slice {xmid} {ypos} {zmid} \n -ss {opfn} \n  '  
-#    dim2_slice = ' -slice {xmid} {ymid} {zpos} \n -ss {opfn} \n  '  
 
-    # This order of coordinates works with PRS+ orientations
-    dim0_slice = ' -slice {ymid} {zmid} {xpos} \n -ss {opfn} \n  '  
-    dim1_slice = ' -slice {ypos} {zmid} {xmid} \n -ss {opfn} \n  '  
-    dim2_slice = ' -slice {ymid} {zpos} {xmid} \n -ss {opfn} \n  '  
+    # Load the json file for the scan
+    jsonPath = mprFn.replace(".nii", ".json").replace(".gz", "")
+    with open(jsonPath, 'r') as f:
+        metadata = json.load(f)
 
+    if "ImageOrientation" in metadata:
+        orientation = metadata['ImageOrientation']
+        print(orientation)
+    else:
+        # want to raise an error here and notify the user to run cubids-add-nifti-info first before running this script
+        pass
 
+    if orientation == "LAS+":   
+        # This order of coordinates works with LAS+ orientations
+        dim0_slice = ' -slice {xpos} {ymid} {zmid} \n -ss {opfn} \n  '  
+        dim1_slice = ' -slice {xmid} {ypos} {zmid} \n -ss {opfn} \n  '  
+        dim2_slice = ' -slice {xmid} {ymid} {zpos} \n -ss {opfn} \n  '  
+    elif orientation == "PSR+":
+        # This order of coordinates works with PRS+ orientations
+        dim0_slice = ' -slice {ymid} {zmid} {xpos} \n -ss {opfn} \n  '  
+        dim1_slice = ' -slice {ypos} {zmid} {xmid} \n -ss {opfn} \n  '  
+        dim2_slice = ' -slice {ymid} {zpos} {xmid} \n -ss {opfn} \n  '  
+    else:
+        print(type(orientation))
 
     # Set up variables 
 #    mprFn = os.path.join(FS_folder, 'mri', 'norm.mgz')          # Want to use the images without the face
@@ -193,9 +280,6 @@ def main():
   
     args = parser.parse_args()
     inDir = args.input_dir
-    
-    # Load freesurfer
-    # module("load", "FreeSurfer/7.1.1")  #----> Does not work!
     
     # --- Output directory --- 
     # outBase = "/home/gudapatis/SLIP_QC/pngOutputs"
