aeeac890efb74aa9e2500eb9d6bce4f2aa325ea1
shreyagudapati9 <138166593+shreyagudapati9@users.noreply.github.com>
2024-04-12 15:23:47 -0400

    Update jobSingleScanPngGenerator.sh

--- png-generation/jobSingleScanPngGenerator.sh
+++ png-generation/jobSingleScanPngGenerator.sh
@@ -9,14 +9,11 @@
 INFN=$1      # file name
 OUTDIR=$2    # output directory for PNGs
 
-#BASE=/mnt/isilon/bgdlab_resnas03/code/manual_smri_qc/
-BASE=/home/gudapatis/SLIP_QC
-
 # Set up conda
 source ${HOME}/miniconda3/etc/profile.d/conda.sh
 conda activate nilearn
 
-time python $BASE/singleScanPngGenerator.py -f $INFN -o $OUTDIR 
+time python singleScanPngGenerator.py -f $INFN -o $OUTDIR 
 
 # Done!
 echo "Job finished running!"
