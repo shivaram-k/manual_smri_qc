cec83dc5011930a7bd4fc5cdf5b1053c7ffbb23b
Shreya Gudapati <shreyagudapati9@gmail.com>
2024-06-05 16:35:59 -0400

    Modified grading-new/imageRatingLib.py

--- grading-new/imageRatingLib.py
+++ grading-new/imageRatingLib.py
@@ -12,6 +12,27 @@ import random
 from IPython.display import clear_output
 import csv
 
+# Function to get user input
+def get_user_input(prompt):
+	while True:
+		user_input = input(prompt).strip()
+		confirmation = input(f"You entered '{user_input}'. Is this correct? (yes/no): ").strip()
+        
+		while True:
+			try:
+				confirmation = confirmation.lower()
+			except:
+				print("Invalid input")
+
+			if confirmation in ['yes', 'no']:
+				break
+			else:
+				confirmation = input(f"You entered '{user_input}'. Is this correct? (yes/no): ").strip()
+		if confirmation == 'yes':
+			return user_input
+		else:
+			print("Let's try again.")
+
 
 # Function to check if grader is in the database
 def check_grader(gradersCSV, grader_name):
@@ -29,25 +50,31 @@ def add_grader(gradersCSV, grader_name, qc_files):
 	with open(gradersCSV, 'a') as csvfile:
 		csvfile.write(grader_name + '\n')
 		
-	# Step 2: Iterate over each batch TSV file
-	for file_name in os.listdir(qc_files):
-		if file_name.startswith('batch_') and file_name.endswith('.tsv'):
-			# Read batch DataFrame from TSV file
-			batch_df = pd.read_csv(os.path.join(qc_files, file_name), sep='\t')
+	# Step 2: Create a directory for the grader if it doesn't exist
+	grader_dir = os.path.join(qc_files, grader_name)
+	if not os.path.exists(grader_dir):
+		os.makedirs(grader_dir)
+		
+		# Copy batch TSV files and add a column in each file to store grades
+		for file_name in os.listdir(qc_files):
+			if file_name.startswith('batch_') and file_name.endswith('.tsv'):
+				# Read batch DataFrame from TSV file
+				batch_df = pd.read_csv(os.path.join(qc_files, file_name), sep='\t')
 
-			new_column_name = f'grade_{grader_name}'
-			if new_column_name not in batch_df.columns:
-				# Add column for new grader to batch DataFrame
-				batch_df[new_column_name] = pd.NA
+				new_column_name = f'grade_{grader_name}'
+				if new_column_name not in batch_df.columns:
+					# Add column for new grader to batch DataFrame
+					batch_df[new_column_name] = pd.NA
 
-				# Save modified batch DataFrame back to TSV file
-				batch_df.to_csv(os.path.join(qc_files, file_name), sep='\t', index=False)
+					# Save modified batch DataFrame to TSV file in grader_dir
+					batch_df.to_csv(os.path.join(grader_dir, file_name), sep='\t', index=False)
+		
 
 # Function to get pending batches for a grader
 def get_pending_batches(grader_name, qc_files):
 
     pending_batches = []
-    for file_name in os.listdir(qc_files):
+    for file_name in os.listdir(os.path.join(qc_files, grader_name)):
         if file_name.startswith('batch_') and file_name.endswith('.tsv'):
             batch_number = int(file_name.split('_')[1].split('.')[0])
             df = pd.read_csv(os.path.join(qc_files, file_name), sep='\t')
@@ -55,16 +82,13 @@ def get_pending_batches(grader_name, qc_files):
                 pending_batches.append(batch_number)
     return sorted(pending_batches)
 
-# Function to check the rating entered and save it
-def save_rating(rating):
-	pass
-		
+	
 	
 # Function to display batch for grading
 def display_batch_for_grading(batch_number, grader_name, qc_files):
 
 	file_name = f'batch_{batch_number:03d}.tsv'
-	full_file_path = os.path.join(qc_files, file_name)
+	full_file_path = os.path.join(qc_files, grader_name, file_name)
 	batch_df = pd.read_csv(full_file_path, sep='\t')
 	grades = []
 	for index, row in batch_df.iterrows():
@@ -86,6 +110,7 @@ def display_batch_for_grading(batch_number, grader_name, qc_files):
 				
 		# Save the rating
 		batch_df.at[index, f'grade_{grader_name}'] = rating
+		batch_df.to_csv(full_file_path, sep='\t', index=False)
 		clear_output()
 
 
@@ -93,10 +118,11 @@ def display_batch_for_grading(batch_number, grader_name, qc_files):
 def start_grading():
 
 	# Enter path to qc_files directory
-	qc_files = input("Enter path to the qc_files or qc_files_synthseg directory : ").strip()
+	qc_files = get_user_input("Enter path to the qc_files or qc_files_synthseg directory : ")
 	
 	# Prompt grader to enter their name
-	grader_name = input("Enter your name: ").strip().lower().replace(' ', '_')
+	grader_name = get_user_input("Enter your name: ")
+	grader_name = grader_name.lower().replace(' ', '_')
 
 	# Check if grader is new or existing
 	gradersCSV = os.path.join(qc_files, "registered_graders.csv")
